17d835b5430389cfa798cabeaf54ebfc4f8bd920
Alex St. Clair <awstclair@gmail.com>
2022-01-27 11:48:18 -0500

    added mile markers to saved csv routes

--- stream_test.py
+++ stream_test.py
@@ -14,11 +14,12 @@ for r in df_list['races']:
     race_info.insert(0,'race',r)
     race_total = race_total.append(race_info, ignore_index=True)
 
+# streamlit multiselect parameters
 default_races = ['twot-2013', 'vickis-death-march', 'vhtrc-waterfall-50k']
 races = race_total['race'].unique()
 selected_races = st.multiselect('Select race', races, default_races)
 df3 = race_total.query('race in @selected_races')
 
 
-fig = px.line(df3, x='cum_distance', y = 'cum_elevation',color ='race', title ='Elevation for races')
+fig = px.line(df3, x='cum_distance', y = 'cum_elevation',color ='race', title ='Elevation for Selected Races')
 st.plotly_chart(fig)
